22f58209db346c8cdf253afe4d06bdb1e9c2531f
Ignacio Abé <ignacio.abe@gmail.com>
2023-06-21 12:37:02 +0200

    ajustes al formulario

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// deshabilita el botón de mandar el formulario después de mandarlo
+// const botón = document.getElementById("manda-formulario");
+// botón.addEventListener("click", function (e) {
+//     this.disabled=true
+// });
