27206ed3a269eeb9a111fea8b16643239e93dbf3
Ignacio Abé <ignacio.abe@gmail.com>
2024-09-10 18:09:44 +0200

    Deja de seguir cambios en carpeta _site

--- _site/main.js
+++
@@ -1,5 +0,0 @@
-// deshabilita el botón de mandar el formulario después de mandarlo
-// const botón = document.getElementById("manda-formulario");
-// botón.addEventListener("click", function (e) {
-//     this.disabled=true
-// });
